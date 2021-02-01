ea4f68d3dd25a9db63321208343b3de3d68f40f3
amaury5432 <amauryval@gmail.com>
2021-02-01 23:15:24 +0100

    upd (rightbar-skills) update (others topics added) and clean

--- src/app/resume/rightbar-skills/rightbar-skills.component.ts
+++ src/app/resume/rightbar-skills/rightbar-skills.component.ts
@@ -11,16 +11,10 @@ import { ResumeService } from '../../services/resume.service';
   styleUrls: ['./rightbar-skills.component.css']
 })
 export class RightbarSkillsComponent implements OnInit {
-  selectedNode!: string;
-  selectedNodeFound!: string;
-  displayAllSkillTopics = true;
 
   themesData!: any;
-  themesTopicOpened = true;
   technicsData!: any;
-  technicsTopicOpened = true;
   toolsData!: any;
-  toolsTopicOpened = false;
 
   faStar = faStar;
   faArrowAltCircleRight = faArrowAltCircleRight;
@@ -34,8 +28,7 @@ export class RightbarSkillsComponent implements OnInit {
         this.themesData = data['1themes'];
         this.technicsData = data['2technics'];
         this.toolsData = data['3tools'];
-        // this.selectedNodeFound = data.selected;
-        // this.checkSelectedNode(data.selected)
+
         console.log(data);
       },
       (error) => {
@@ -47,27 +40,5 @@ export class RightbarSkillsComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
-  switchSkillCategoryDisplay(category: string): void {
-    if (category === 'themes') {
-      this.themesTopicOpened = !this.themesTopicOpened;
-    }
-    if (category === 'technics') {
-      this.technicsTopicOpened = !this.technicsTopicOpened;
-    }
-    if (category === 'tools') {
-      this.toolsTopicOpened = !this.toolsTopicOpened;
-    }
-  }
-
-  // checkSelectedNode(selectedNode: string | null): void {
-  //   if (selectedNode === null) {
-  //     this.displayAllSkillTopics = true;
-  //     this.selectedNode = '';
-  //   } else {
-  //     this.displayAllSkillTopics = false;
-  //     this.selectedNode = selectedNode;
-  //   }
-  // }
 
 }
