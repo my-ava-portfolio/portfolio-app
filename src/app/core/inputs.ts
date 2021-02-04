6e60f0225c899c6b300903ba167ab50764bfc6fc
amaury5432 <amauryval@gmail.com>
2021-02-04 20:55:57 +0100

    upd (input) prepare font awesome refactoring

--- src/app/core/inputs.ts
+++ src/app/core/inputs.ts
@@ -1,9 +1,29 @@
-// export const apiBaseUrl = 'https://find-my-path.herokuapp.com';
+import { faPrint, faStar, faArrowAltCircleDown, faAddressCard, faImages, faBookOpen, faGlobeEurope, faQuestionCircle, faMapMarkerAlt, faArrowAltCircleUp, faGlobe, faFilePdf, faUserGraduate, faLanguage } from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
+
+
 export const apiBaseUrl = 'http://127.0.0.1:5000/';
 export const apiImgUrl = `${apiBaseUrl}/images/logo/`;
 
 export const navBarTitle = "Amaury Valorge - Portfolio"
 
+
+export const resumeIcon = faAddressCard;
+export const galleryIcon = faImages;
+export const notesIcon = faBookOpen;
+export const githubIcon = faGithub;
+export const topicIcon = faGlobeEurope;
+export const helpIcon = faQuestionCircle;
+export const locationIcon = faMapMarkerAlt;
+export const arrowUpIcon = faArrowAltCircleUp;
+export const websiteIcon = faGlobe;
+export const pdfFileIcon = faFilePdf;
+export const degreeIcon = faUserGraduate;
+export const languageIcon = faLanguage;
+export const skillIcon = faStar;
+export const arrowDownIcon = faArrowAltCircleDown;
+export const printIcon = faPrint;
+export const linkedinIcon = faLinkedinIn;
 
 export const homePage: any = {
   route: 'home',
@@ -16,23 +36,29 @@ export const pages: any = [
   {
     route: 'resume',
     title: 'Profil',
-    status: false
+    status: false,
+    icon: resumeIcon
   },
   {
     route: 'gallery',
     title: 'Galerie',
-    status: false
+    status: false,
+    icon: galleryIcon
   },
   {
     route: 'notes',
     title: 'Notes',
-    status: false
+    status: false,
+    icon: notesIcon
   },
   {
     route: 'github',
     title: 'Github',
-    status: false
+    status: false,
+    icon: githubIcon
   }
 ];
 
 export const pagesObject: any = pages.reduce((a: any, x: any) => ({...a, [x.route]: x}), {})
+
+
