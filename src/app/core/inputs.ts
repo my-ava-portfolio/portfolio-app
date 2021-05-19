bb8fbf7357ee42b4e506b7cc37ea61f4f3da13dc
amaury5432 <amauryval@gmail.com>
2021-05-19 21:06:15 +0200

    upd (inputs.ts) add new icon and main url improved

--- src/app/core/inputs.ts
+++ src/app/core/inputs.ts
@@ -1,20 +1,23 @@
-import { faAngleRight, faProjectDiagram, faExpand, faTools, faMobileAlt, faTags, faCogs, faStepBackward, faStepForward, faPrint, faStar, faArrowAltCircleDown, faPhone, faFilter, faGlobeEurope, faQuestionCircle, faMapMarkerAlt, faArrowAltCircleUp, faGlobe, faFilePdf, faUserGraduate, faLanguage } from '@fortawesome/free-solid-svg-icons';
+import { faExclamationCircle, faBug, faSpinner, faAngleRight, faProjectDiagram, faExpand, faTools, faMobileAlt, faTags, faCogs, faStepBackward, faStepForward, faPrint, faStar, faArrowAltCircleDown, faPhone, faFilter, faGlobeEurope, faQuestionCircle, faMapMarkerAlt, faArrowAltCircleUp, faGlobe, faFilePdf, faUserGraduate, faLanguage } from '@fortawesome/free-solid-svg-icons';
 import { faAppStore, faYoutube, faPython, faGithub, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
 
 import { faEnvelope, faChartBar, faAddressCard, faImages, faMap, faFileAlt } from '@fortawesome/free-regular-svg-icons';
 
 import { apiBaseUrl } from '../../app/core/constants';
 
-export const apiUrl = apiBaseUrl;
+export const apiUrl = apiBaseUrl + 'api/v1/portfolio/';
+export const apiCoreUrl = apiBaseUrl + 'api/v1/core/';
 export const apiLogoUrl = `./assets/logo/`;
 export const apiMapsUrl = `./assets/maps/`;
 export const jupyterNotebookUrl = 'https://amauryval.github.io/';
 export const githubUrl = 'https://github.com/amauryval/';
 export const navBarTitle = 'Amaury Valorge - Portfolio';
+export const githubBugIssueUrl = 'https://github.com/amauryval/portfolio/issues/new?assignees=amauryval&labels=bug&template=bug_report.md&title=';
+export const githubQuestionUrl = 'https://github.com/amauryval/portfolio/issues/new?assignees=amauryval&labels=question&template=question-.md&title=';
+export const githubEnhancementUrl = 'https://github.com/amauryval/portfolio/issues/new?assignees=amauryval&labels=enhancement&template=feature_request.md&title=';
 
 export const minWidthLandscape = 1024;
 export const minHeightLandscape = 768;
-
 
 export const resumeIcon = faAddressCard;
 export const galleryIcon = faImages;
@@ -22,6 +25,7 @@ export const notesIcon = faFileAlt;
 export const githubIcon = faGithub;
 export const topicIcon = faGlobeEurope;
 export const helpIcon = faQuestionCircle;
+export const exclamationIcon = faExclamationCircle;
 export const locationIcon = faMapMarkerAlt;
 export const arrowUpIcon = faArrowAltCircleUp;
 export const websiteIcon = faGlobe;
@@ -45,11 +49,12 @@ export const centerIcon = faExpand;
 export const filterIcon = faFilter;
 export const methodoIcon = faProjectDiagram;
 export const nextIcon = faAngleRight;
-
+export const loadingIcon = faSpinner;
 export const chartItemIcon = faChartBar;
 export const videoItemIcon = faYoutube;
 export const appItemIcon = faAppStore;
 export const toolItemIcon = faTools;
+export const bugIcon = faBug;
 
 // font awesome unicode
 export const trainIconUnicode = '\uf238';
