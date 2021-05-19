508d3631be90ef7bdd613b5106d375148803c75e
amaury5432 <amauryval@gmail.com>
2021-05-19 21:19:33 +0200

    upd (header) github issues icon

--- src/app/background/header/header.component.ts
+++ src/app/background/header/header.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Router } from '@angular/router';
+import { Location } from '@angular/common';
+
 import { navBarTitle, homePage, pages } from '../../core/inputs';
+import { helpIcon, exclamationIcon, bugIcon } from '../../core/inputs';
+import { githubBugIssueUrl, githubEnhancementUrl, githubQuestionUrl } from '../../core/inputs';
 
 
 @Component({
@@ -9,14 +14,41 @@ import { navBarTitle, homePage, pages } from '../../core/inputs';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  currentPage!: string;
 
   homePage: any = homePage;
   navBarTitle: string = navBarTitle;
   portfolioPages: any = pages;
 
-  constructor() { }
+  bugIcon = bugIcon;
+  helpIcon = helpIcon;
+  exclamationIcon = exclamationIcon;
+  GithubBugIssueLink = githubBugIssueUrl;
+  GithubQuestionIssueLink = githubQuestionUrl;
+  GithubEnhancementIssueLink = githubEnhancementUrl;
+
+  contactPopupTitle = 'Contact';
+  contactBugMessage = 'Un bug ?';
+  contactHelpMessage = "Besoin d'aide ?"
+  contactFeatureMessage = 'Des améliorations ?';
+
+  issueSufixTitle = ' page:';
+
+  constructor(
+    private router: Router,
+    private location: Location,
+  ) {
+
+    // to get the current page opened and adapt content regarding orientation
+    // todo: check if needed
+    this.router.events.subscribe(_ => {
+      this.currentPage = this.location.path();
+    });
+
+   }
 
   ngOnInit(): void {
+    this.currentPage = this.location.path();
   }
 
 }
