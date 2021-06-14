1def8152f0a2ef589f9d68dc6271d5d017e264fd
amaury5432 <amauryval@gmail.com>
2021-06-14 22:45:55 +0200

    upd (app) add commented line to disable the heroku waiter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -44,6 +44,7 @@ export class AppComponent implements OnInit {
     this.apiService.apiHealth.subscribe(data => {
       this.apiStatus = data;
 
+      // this.apiStatus = 'Ready'
       if (this.apiStatus === 'Ready') {
         this.apiOff = false;
         // to not check all the time
