eefe2d4a77687f0cc8757e7d023f7e2b78da153b
amaury5432 <amauryval@gmail.com>
2021-10-10 19:03:02 +0200

    upd (constants.ts) ava-api applied

--- src/app/core/constants.ts
+++ src/app/core/constants.ts
@@ -1,2 +1,5 @@
 // export const apiBaseUrl = 'http://127.0.0.1:5000/';
-export const apiBaseUrl = 'https://ava-api.herokuapp.com/';
+// export const apiBaseUrl = 'https://ava-api.herokuapp.com/';
+// export const apiBaseUrl = 'http://127.0.0.1:8080/';
+export const apiBaseUrl = 'https://amaury-valorge.com/';
+
