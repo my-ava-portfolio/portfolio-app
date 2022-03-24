c672a09176d9b0a9fc0b36e1829b0e9549605e89
amaury5432 <amauryval@gmail.com>
2022-03-24 19:44:10 +0100

    fix (toolbox) style + drag and drop

--- src/app/core/points_svg_layer.ts
+++ src/app/core/points_svg_layer.ts
@@ -2,10 +2,74 @@ import * as d3 from 'd3';
 import * as L from 'leaflet';
 
 
+
+export class Point {
+
+  id: string = `feat_${Date.now()}`;
+  position!: number;
+  private _x!: number;
+  private _y!: number;
+
+  constructor(
+    position: number,
+    coordsX: number,
+    coordsY: number,
+  ) {
+    this.position = position;
+    this._x = coordsX;
+    this._y = coordsY;
+  }
+
+  get x(): number {
+    return this._x;
+  }
+  set x(coord: number) {
+    this._x = coord;
+  }
+  get y(): number {
+    return this._y;
+  }
+  set y(coord: number) {
+    this._y = coord;
+  }
+
+  updateCoord(x: number, y: number) {
+    this._x = x;
+    this._y = y;
+  }
+
+  to_array(): number[] {
+    return [this._x, this._y];
+  };
+
+  to_wkt(): string {
+    return `POINT(${this._x} ${this._y})`;
+  };
+
+  getProperties(): any {
+    let properties: any = {};
+
+    let propertiesKeys: string[] = Object.getOwnPropertyNames(this);
+    propertiesKeys.forEach(
+      (property: any): void => {
+        properties[property] = this.getattr(this, property)
+      }
+    );
+    return properties;
+  }
+
+  private getattr(obj: any, prop: string, def = null ): any {
+    return prop in obj ? obj[prop] : def;
+  }
+
+}
+
+
 export class PointsSvgLayerOnLeaflet {
 
   private mapContainer: any;
   private layerName: string;
+  private pointCount: number = -1
 
   points: any[] = [];
 
@@ -16,14 +80,9 @@ export class PointsSvgLayerOnLeaflet {
   }
 
 
-  addPoints(newPoint: any): void {
-    let idValue: number = Date.now();
-    let pointProperties: any = {
-      id: idValue,
-      coords : newPoint,
-
-    }
-    this.points.push(pointProperties);
+  addPoints(coordinates: any): void {
+    this.pointCount += 1
+    this.points.push(new Point(this.pointCount, coordinates.x, coordinates.y));
     this.buildPointsLayer()
   };
 
@@ -36,44 +95,47 @@ export class PointsSvgLayerOnLeaflet {
       .data(this.points)
       .enter()
       .append("circle")
-      .attr("r", 14)
+      .style("r", 14)
       .style("fill", "red")
+      .attr("id", (d: any) => {return d.id})
       .attr("class", "cursor")
       .attr("stroke", "red")
-      .attr("stroke-width", 3)
+      .attr("stroke-width", "3px")
       .attr("fill-opacity", .4)
-      .attr('transform', (d: any) => {
+      .attr('transform', (point: any, event: any) => {
+        console.log(point, event)
         return 'translate(' +
-          this.mapContainer.latLngToLayerPoint([d.coords.x, d.coords.y]).x + ',' +
-          this.mapContainer.latLngToLayerPoint([d.coords.x, d.coords.y]).y + ')';
+          this.mapContainer.latLngToLayerPoint([point.x, point.y]).x + ',' +
+          this.mapContainer.latLngToLayerPoint([point.x, point.y]).y + ')';
       });
 
     let dragHandler = d3.drag()
-      .on("drag", (e: any, d: any) => {
+      .on("start", (e: any, d: any) => {
         this.mapContainer.dragging.disable();
-        let coordsUpdated = this.mapContainer.layerPointToLatLng([e.x, e.y])
 
-        let pointToUpdate: number = this.points.findIndex(
-          (node: any): boolean =>
-              node.id === d.id
-        );
-        this.points[pointToUpdate].coords = {
-          x: coordsUpdated.lat,
-          y: coordsUpdated.lng
-        };;
+      })
+      .on("drag", (e: any, d: any) => {
+
+        let coordsUpdated = this.mapContainer.layerPointToLatLng([e.sourceEvent.clientX, e.sourceEvent.clientY])
+
+        let currentPoints: Point = e.subject;
+        currentPoints.x = coordsUpdated.lat
+        currentPoints.y = coordsUpdated.lng
+
         this.updateMapLayer()
       })
       .on("end", (e: any, d: any) => {
         this.mapContainer.dragging.enable();
-    });
+      });
+
     dragHandler(
       d3.select('#' + this.layerName + '-container')
-      .selectAll("circle")
+        .selectAll("circle")
     );
 
     this.mapContainer.on("moveend", this.updateMapLayer.bind(this))
 
-    // this.updateMapLayer()
+    this.updateMapLayer()
     this.initTooltip()
 
   };
@@ -83,9 +145,49 @@ export class PointsSvgLayerOnLeaflet {
     d3.selectAll("[id^='tooltip-" + this.layerName + "']").remove()
 
     if (deletePoints) {
-      console.log('popo')
       this.points = [];
     }
+  };
+
+  removePointById(idToRemove: string): void {
+
+    this.points.forEach((element, index) => {
+      if (element.id === idToRemove) {
+        this.points.splice(index, 1);
+      }
+    });
+    this.buildPointsLayer()
+  };
+
+  getPointById(idToSelect: string): Point {
+    let pointFound!: Point;
+
+    this.points.forEach((element: Point, index: number) => {
+      if (element.id === idToSelect) {
+        pointFound = this.points[index];
+      }
+    });
+
+    return pointFound;
+  };
+
+  highLightPointById(idToSelect: string): void {
+    console.log("pouette", d3.select('#' + idToSelect))
+    d3.select('#' + idToSelect)
+    .transition()
+    .duration(1000)
+    .ease(d3.easeElastic)
+      .style("r", 25)
+
+  };
+
+  unHighLightPointById(idToSelect: string): void {
+    d3.select('#' + idToSelect)
+    .transition()
+    .duration(1000)
+    .ease(d3.easeElastic)
+      .style("r", 14)
+
   };
 
   private initSvgLayer(): void {
@@ -99,13 +201,16 @@ export class PointsSvgLayerOnLeaflet {
   };
 
   private updateMapLayer(): void {
+
+    console.log(this.points)
     d3.select('#' + this.layerName + '-container')
       .selectAll("circle")
       .attr('transform', (d: any) => {
+        console.log([d.x, d.y])
         return 'translate(' +
-          this.mapContainer.latLngToLayerPoint([d.coords.x, d.coords.y]).x + ',' +
-          this.mapContainer.latLngToLayerPoint([d.coords.x, d.coords.y]).y + ')';
-      });
+          this.mapContainer.latLngToLayerPoint([d.x, d.y]).x + ',' +
+          this.mapContainer.latLngToLayerPoint([d.x, d.y]).y + ')';
+      })
   }
 
   initTooltip(): any {
@@ -119,8 +224,8 @@ export class PointsSvgLayerOnLeaflet {
       .attr("id", (d: any) => { return "tooltip-" + this.layerName + d.id })
       .style("background-color", "white")
       .style("border", "solid")
-      .style("border-width", "2px")
-      .style("border-radius", "5px")
+      .attr("stroke", "red")
+      .style("border-width", "3px")
       .style("padding", "5px");
 
     d3.select('#' + this.layerName + '-container')
@@ -135,33 +240,31 @@ export class PointsSvgLayerOnLeaflet {
   }
 
   mouseOver(e: any, d: any): void {
-    d3.select("#tooltip-" + this.layerName + d.id )
+    d3.select("#tooltip-" + this.layerName + d.id)
       .style("opacity", 1)
 
     d3.select(e.currentTarget)
-      .style("stroke", "black")
       .style("opacity", 1)
   }
 
   mouseMove(e: any, d: any): void {
-    d3.select("#tooltip-" + this.layerName + d.id )
-      .html(`POINT(${d.coords.x} ${d.coords.y})`)
+    d3.select("#tooltip-" + this.layerName + d.id)
+      .html(d.to_wkt())
       .style("left", e.x + 15 + "px")
       .style("top", e.y + 15 + "px")
+
     console.log("aaa")
   }
 
   mouseLeave(e: any, d: any): void {
-    d3.select("#tooltip-" + this.layerName+ d.id )
+    d3.select("#tooltip-" + this.layerName + d.id)
       .style("opacity", 0)
 
-    d3.select(e.currentTarget)
-      .style("stroke", "none")
   }
 
   mouseClick(e: any, d: any): void {
     d3.select("#tooltip-" + this.layerName + d.id)
-      .html(`POINT(${d.coords.x} ${d.coords.y})`)
+      .html(d.to_wkt())
       .style("left", e.x + 15 + "px")
       .style("top", e.y + 15 + "px")
       .style("opacity", 1)
@@ -169,5 +272,6 @@ export class PointsSvgLayerOnLeaflet {
     console.log("aaa")
   }
 
-
 }
+
+
--- src/app/home/toolbox/toolbox.component.ts
+++ src/app/home/toolbox/toolbox.component.ts
@@ -1,34 +1,172 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { ActivatedRoute } from '@angular/router';
-import { ResumeService } from 'src/app/services/resume.service';
-import { helpIcon, toolsIcon, locationIcon, lineIcon, PolygonIcon } from '../../core/inputs';
 
+import { helpIcon, removeIcon, toolsIcon, locationIcon, lineIcon, PolygonIcon, editIcon, addIcon } from '../../core/inputs';
+
+import { PointsSvgLayerOnLeaflet } from '../../core/points_svg_layer';
+import { Point } from '../../core/points_svg_layer';
+
+import { LinesSvgLayerOnLeaflet } from '../../core/lines_svg_layer';
+import { Subscription } from 'rxjs/internal/Subscription';
+import { MapService } from 'src/app/services/map.service';
 
 @Component({
   selector: 'app-toolbox',
   templateUrl: './toolbox.component.html',
   styleUrls: ['./toolbox.component.scss']
 })
-export class ToolboxComponent implements OnInit {
+export class ToolboxComponent implements OnInit  {
   toolsIcon = toolsIcon;
-  locationIcon = locationIcon;
+  pointIcon = locationIcon;
   lineIcon = lineIcon;
   PolygonIcon = PolygonIcon;
+  editIcon = editIcon;
+  addIcon = addIcon;
+  removeIcon = removeIcon;
+  helpIcon = helpIcon;
+
+  geomTypesList: any[] = [
+    {
+      name: 'points',
+      icon: this.pointIcon
+    },
+    {
+      name: 'lines',
+      icon: this.lineIcon
+    },
+    {
+      name: 'polygons',
+      icon: this.PolygonIcon
+    },
+  ];
 
   sidebarCollapsed: boolean = false;
+
+  editStatus!: any;
+  editModeEnabled: boolean = false;
+  geomTypeButton!: string;
+  geomInfo!: Point;
+
+  pointsLayer!: PointsSvgLayerOnLeaflet
+  pointsMapped: any[] = []
+  linesLayer!: LinesSvgLayerOnLeaflet
+  linessMapped: any[] = []
+
+  mapContainer!: any;
+
+  mapContainerSubscription!: Subscription;
+  getCoordsMapSubscription!: Subscription;
+
+  newPointsSvgMapSubscription!: Subscription;
+  removePointsSvgMapSubscription!: Subscription;
 
   constructor(
     private activatedRoute: ActivatedRoute,
     private titleService: Title,
-    private resumeService: ResumeService
+    private mapService: MapService,
   ) {
 
     this.titleService.setTitle(this.activatedRoute.snapshot.data.title);
 
+    this.mapContainerSubscription = this.mapService.mapContainer.subscribe(
+      (map: any) => {
+        this.mapContainer = map;
+
+        this.pointsLayer = new PointsSvgLayerOnLeaflet(this.mapContainer, "sandboxPoints");
+        this.pointsLayer.buildPointsLayer();
+
+        // this.linesLayer = new LinesSvgLayerOnLeaflet(this.mapContainer, "sandboxLines")
+        // this.linesLayer.addLines();
+
+      }
+    );
+
+    this.getCoordsMapSubscription = this.mapService.newCoords.subscribe(
+      (coords: any) => {
+
+        let currentGeomTypeEdited = this.getCurrentGeomType();
+        console.log(currentGeomTypeEdited)
+
+        if ( currentGeomTypeEdited === 'points' ) {
+          this.pointsLayer.addPoints(coords)
+          this.pointsMapped = this.pointsLayer.points
+
+        } else if ( currentGeomTypeEdited === 'lines' ) {
+          this.linesLayer.addPoints(0, coords);
+          this.pointsMapped = []
+        }
+      }
+    );
+
    }
 
   ngOnInit(): void {
+    this.resetEditModeDefaultValues(false);
+    this.mapService.getMapContainer();
+    this.displayGeomToolbar('points')
+  }
+
+  editModeSelector(geomType: string): void {
+
+    if (this.editModeEnabled) {
+      // desactivate
+      this.resetEditModeDefaultValues(false);
+    } else {
+      // activate
+      this.resetEditModeDefaultValues(true);
+      this.editStatus[geomType] = true;
+    }
+  }
+
+  resetEditModeDefaultValues(setEditMode: boolean): void {
+    this.switchEditMode(setEditMode);
+
+    this.editStatus = {
+      "points": false,
+      "lines": false,
+      "polygons": false,
+    }
+  }
+
+  getCurrentGeomType(): string {
+    return Object.keys(this.editStatus).filter((key: string) => this.editStatus[key])[0];
+  }
+
+  switchEditMode(status: boolean): void {
+    this.editModeEnabled = status;
+  }
+
+  removeGeom(geomType: string, geomId: string): void {
+    if (geomType === 'point') {
+      this.pointsLayer.removePointById(geomId)
+    }
+  }
+
+  displayGeomToolbar(geomType: string): void {
+    this.geomTypeButton = geomType;
+  }
+
+  getGeomInfo(geomId: string): void {
+    if (this.getCurrentGeomType() === 'points') {
+
+      this.geomInfo = this.pointsLayer.getPointById(geomId);
+      console.log(this.geomInfo)
+    }
+  }
+
+  highLightGeom(geomId: string): void {
+    if (this.getCurrentGeomType() === 'points') {
+
+      this.pointsLayer.highLightPointById(geomId);
+    }
+  }
+
+  unHighLightGeom(geomId: string): void {
+    if (this.getCurrentGeomType() === 'points') {
+
+      this.pointsLayer.unHighLightPointById(geomId);
+    }
   }
 
 }
