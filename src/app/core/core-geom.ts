6a66c829df71ab6f67d1170bf89e0594351a1706
amaury5432 <amauryval@gmail.com>
2022-03-27 21:06:59 +0200

    upd (core-geom) add geom handlers

--- src/app/core/core-geom.ts
+++ src/app/core/core-geom.ts
@@ -1,36 +1,25 @@
-export class coordinates {
+export class CoordinatesType {
   x!: number;
   y!: number;
-  constructor() {}
 }
 
-export class Point {
-  id: string = `feat_${Date.now()}`;
-  _name!: string;
-  _tag: string = "noTag"
-  _color: string = "#FF0000"
-  _editStatus: boolean = false;
-  private _coords!: coordinates;
+export class CommonProperties {
+
+  private _name!: string;
+  private _tag: string = "noTag"
+  private _editStatus: boolean = false;
 
   constructor(
     name: string,
-    coords: coordinates,
   ) {
     this._name = name;
-    this._coords = coords;
   }
 
-  get x(): number {
-    return this._coords.x;
+  get edited(): boolean {
+    return this._editStatus;
   }
-  set x(coord: number) {
-    this._coords.x = coord;
-  }
-  get y(): number {
-    return this._coords.y;
-  }
-  set y(coord: number) {
-    this._coords.y = coord;
+  set edited(editStatus: boolean) {
+    this._editStatus = editStatus;
   }
 
   get tag(): string {
@@ -47,24 +36,6 @@ export class Point {
     this._name = nameValue;
   }
 
-  get color(): string {
-    return this._color;
-  }
-  set color(colorValue: string) {
-    this._color = colorValue;
-  }
-
-  get edited(): boolean {
-    return this._editStatus;
-  }
-  set edited(editStatus: boolean) {
-    this._editStatus = editStatus;
-  }
-
-  toWkt(): string {
-    return `POINT(${this._coords.x} ${this._coords.y})`;
-  };
-
   getProperties(): any {
     let properties: any = {};
 
@@ -76,6 +47,61 @@ export class Point {
     );
     return properties;
   }
+
+}
+
+// points
+export class PointGeom extends CommonProperties {
+  _x!: number;
+  _y!: number;
+
+  constructor(
+    name: string,
+    coords: CoordinatesType
+  ) {
+    super(name);
+    this._x = coords.x;
+    this._y = coords.y;
+  }
+
+  get x(): number {
+    return this._x;
+  }
+  set x(coord: number) {
+    this._x = coord;
+  }
+  get y(): number {
+    return this._y;
+  }
+  set y(coord: number) {
+    this._y = coord;
+  }
+
+  toWkt(): string {
+    return `POINT(${this.x} ${this.y})`;
+  };
+
+}
+
+export class Point extends PointGeom {
+  id: string = `feat_${Date.now()}`;
+  private _color: string = "#FF0000"
+
+  constructor(
+    name: string,
+    coords: CoordinatesType,
+  ) {
+    super(name, coords);
+  }
+
+  get color(): string {
+    return this._color;
+  }
+  set color(colorValue: string) {
+    this._color = colorValue;
+  }
+  // TODO add more styles getter and setter
+
 }
 
 
