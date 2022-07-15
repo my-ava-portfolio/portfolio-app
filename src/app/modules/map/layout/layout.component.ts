0bd7a18fc12fa9e0e0d3ae6839259f52ccd6a85f
amaury5432 <amauryval@gmail.com>
2022-07-15 13:34:04 +0200

    upd (layout map) setMapControler observable improved

--- src/app/modules/map/layout/layout.component.ts
+++ src/app/modules/map/layout/layout.component.ts
@@ -41,26 +41,27 @@ export class LayoutComponent implements OnInit, AfterViewInit, OnDestroy {
   ) {
 
     this.mapScaleDivSubscription = this.mapService.setMapControler.subscribe(
-      (_: boolean) => {
-        // TODO clean observable
+      (status: boolean) => {
+        if (status) {
+          this.mapService.setControlToMap("miniMap")
+          const divOverview: any = window.document.getElementById('overview-map');
+          divOverview.appendChild(
+            window.document.getElementsByClassName("ol-overviewmap ol-custom-overviewmap")[0]
+          )
 
-        this.mapService.setControlToMap("miniMap")
-        const divOverview: any = window.document.getElementById('overview-map');
-        divOverview.appendChild(
-          window.document.getElementsByClassName("ol-overviewmap ol-custom-overviewmap")[0]
-        )
+          this.mapService.setControlToMap("scale")
+          const divScale: any = window.document.getElementById('legend-scale');
+          divScale.appendChild(
+            window.document.getElementsByClassName("ol-scale-line ol-unselectable")[0]
+          )
 
-        this.mapService.setControlToMap("scale")
-        const divScale: any = window.document.getElementById('legend-scale');
-        divScale.appendChild(
-          window.document.getElementsByClassName("ol-scale-line ol-unselectable")[0]
-        )
+          this.mapService.setControlToMap("attribution")
+          const divAttribution: any = window.document.getElementById('attribution')
+          divAttribution.appendChild(
+            window.document.getElementsByClassName("ol-attribution ol-unselectable ol-control ol-uncollapsible")[0]
+          )
+        }
 
-        this.mapService.setControlToMap("attribution")
-        const divAttribution: any = window.document.getElementById('attribution')
-        divAttribution.appendChild(
-          window.document.getElementsByClassName("ol-attribution ol-unselectable ol-control ol-uncollapsible")[0]
-        )
       }
     );
 
