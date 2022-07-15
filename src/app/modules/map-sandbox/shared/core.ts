120f840251677b9d8a42426fcf61a968abac539c
amaury5432 <amauryval@gmail.com>
2022-07-16 01:26:04 +0200

    upd (map-sandbox/core) drawInteraction improved

--- src/app/modules/map-sandbox/shared/core.ts
+++ src/app/modules/map-sandbox/shared/core.ts
@@ -1,3 +1,4 @@
+import {Polygon}  from 'ol/geom';
 import { v4 as uuidv4 } from 'uuid';
 
 import { Draw, Modify, Snap } from 'ol/interaction';
@@ -5,6 +6,8 @@ import VectorLayer from 'ol/layer/Vector';
 
 import Map from 'ol/Map';
 import VectorSource from 'ol/source/Vector';
+import Feature from 'ol/Feature';
+import LinearRing from 'ol/geom/LinearRing';
 
 
 export class DrawInteraction {
@@ -12,6 +15,11 @@ export class DrawInteraction {
   private draw!: Draw;
   private snap!: Snap;
   private modifier!: Modify;
+
+  // var for polygon holes
+  private holePolygonDrawingStatus = false;
+  private polygonIntersected!: Feature | undefined;
+  private coordsLength!: number;
 
   vectorLayer: VectorLayer<any>;
   sourceFeatures: VectorSource;
@@ -32,7 +40,8 @@ export class DrawInteraction {
 
     this.draw = new Draw({
       type: geomType,
-      source: this.vectorLayer.getSource()
+      stopClick: true,
+      source: this.vectorLayer.getSource(),
     });
 
     this.map.addInteraction(this.draw);
@@ -41,6 +50,11 @@ export class DrawInteraction {
 
     this.draw.on('drawstart', this.onDrawStart.bind(this));
     this.draw.on('drawend', this.onDrawEnd.bind(this));
+
+    this.draw.on('drawabort', () => {
+      console.log('draw aborted');
+    });
+
   }
 
   disableDrawing(): void {
@@ -53,20 +67,67 @@ export class DrawInteraction {
   }
 
   onDrawStart(e: any): void {
+
+    // to build hole on polygon
+    if (e.feature.getGeometry()?.getType() === "Polygon") {
+      this.vectorLayer.getSource().forEachFeatureIntersectingExtent(e.feature.getGeometry().getExtent(), (feature: Feature | undefined) => {
+        this.polygonIntersected = feature;
+      });
+
+      if (this.polygonIntersected !== undefined) {
+        this.holePolygonDrawingStatus = true;
+        const polygonIntersectedGeom: any = this.polygonIntersected.getGeometry()
+        this.coordsLength = polygonIntersectedGeom.getCoordinates().length;
+        e.feature.getGeometry().on('change', this.onGeomChange.bind(this));
+      }
+    }
   }
 
   onDrawEnd(e: any): void {
-    const featureCount = this.sourceFeatures.getFeatures().length
-    const uuid = uuidv4()
-    e.feature.setId(uuid)
-    e.feature.setProperties({
-      'name': 'feature_' + featureCount,
-      'geom_type': e.feature.getGeometry()?.getType(),
-      'created_at': new Date().toISOString()
-    })
+
+    if (this.holePolygonDrawingStatus) {
+      setTimeout(() => {
+        this.sourceFeatures.removeFeature(e.feature);
+      }, 5);
+
+      if (this.polygonIntersected !== undefined) {
+        this.polygonIntersected.setProperties({
+          'updated_at': new Date().toISOString()
+        })
+      }
+      this.polygonIntersected = undefined;
+      this.holePolygonDrawingStatus = false
+    }
+
+    if (!this.holePolygonDrawingStatus) {
+      const featureCount = this.sourceFeatures.getFeatures().length
+      const uuid = uuidv4()
+      e.feature.setId(uuid)
+      e.feature.setProperties({
+        'name': 'feature_' + featureCount,
+        'geom_type': e.feature.getGeometry()?.getType(),
+        'created_at': new Date().toISOString(),
+        'updated_at': new Date().toISOString()
+      })
+    }
+
+
   }
 
   onGeomChange(e: any): void {
+    //Get hole coordinates for polygon
+    let linearRing = new LinearRing(e.target.getCoordinates()[0]);
+
+    if (this.polygonIntersected !== undefined) {
+      const polygonIntersectedGeom: any = this.polygonIntersected.getGeometry()
+      let coordinates = polygonIntersectedGeom.getCoordinates();
+      let geom = new Polygon(coordinates.slice(0, this.coordsLength));
+
+      //Add hole coordinates to polygon and reset the polygon geometry
+      geom.appendLinearRing(linearRing);
+      console.log(linearRing)
+      this.polygonIntersected.setGeometry(geom);
+    }
   }
 
 
