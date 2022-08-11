771761ac27b6b182c8616a104285db2f272f603b
amaury5432 <amauryval@gmail.com>
2022-08-11 21:40:06 +0200

    upd (sandbox/feature) remove popup func because useless

--- src/app/modules/map-sandbox/layer-components/feature/feature.component.ts
+++ src/app/modules/map-sandbox/layer-components/feature/feature.component.ts
@@ -1,5 +1,5 @@
 import { Component, ElementRef, EventEmitter, Input, OnInit, Output, SimpleChanges } from '@angular/core';
-import { faXmark } from '@fortawesome/free-solid-svg-icons';
+import { faExpand, faXmark } from '@fortawesome/free-solid-svg-icons';
 import { faClone } from '@fortawesome/free-regular-svg-icons';
 import { Feature } from 'ol';
 import { Subject, Subscription } from 'rxjs';
@@ -24,32 +24,31 @@ export class FeatureComponent implements OnInit {
 
   disabledIcon = faXmark;
   duplicateIcon = faClone;
+  centerIcon = faExpand;
 
-  featuresPopupsProperties!: any
+  currentFillColor!: string;
+  currentStrokeColor!: string;
+  currentStrokeWidth!: number;
 
-  featuresDisplayedObservable = new Subject<Feature>()
-  featuresDisplayedSubscription!: Subscription;
+  displayPopup: boolean = true;
 
   constructor(
     private elementRef: ElementRef
   ) {
-    this.featuresDisplayedSubscription = this.featuresDisplayedObservable.subscribe(
-      (feature: Feature) => {
-        this.resetFeaturesPopups()
-        this.buildFeaturesPopups(feature)
-      }
-    )
+
 
   }
 
   ngOnInit(): void {
     this.moveToastsFeaturesToBody()
+
+    this.currentFillColor = this.feature.get('fill_color')
+    this.currentStrokeColor = this.feature.get('stroke_color')
+    this.currentStrokeWidth = this.feature.get('stroke_width')
   }
 
   ngOnDestroy(): void {
-    this.featuresDisplayedSubscription.unsubscribe();
     this.elementRef.nativeElement.remove();
-
   }
 
   ngOnChanges(changes: SimpleChanges) {
@@ -57,8 +56,6 @@ export class FeatureComponent implements OnInit {
     const featureIdSelected = changes.currentFeatureIdSelected.currentValue
     if (this.feature.getId() === featureIdSelected) {
       this.selectFeature()
-    } else {
-      this.resetFeaturesPopups()
     }
 
   }
@@ -68,9 +65,7 @@ export class FeatureComponent implements OnInit {
     const featureId = this.feature.getId()
     if (typeof featureId === 'string') {
       this.featureIdSelected.emit(featureId)
-      // this.featuresDisplayedObservable.next(this.feature)
-      this.resetFeaturesPopups()
-      this.buildFeaturesPopups(this.feature)
+      this.displayPopup = true;
     }
   }
 
@@ -82,35 +77,8 @@ export class FeatureComponent implements OnInit {
     this.duplicateFeatureEvent.emit(this.feature.getId())
   }
 
-  resetFeaturesPopups(): void {
-    let toastFeatureDiv = document.getElementById("featureToasts");
-
-    if (toastFeatureDiv !== null) {
-
-      toastFeatureDiv.remove()
-    }
-    this.featuresPopupsProperties = null
-  }
-
-  buildFeaturesPopups(feature: Feature): any {
-
-    // get wkt regarding selected projection
-    const geomFeature = feature.getGeometry();
-    if (geomFeature !== undefined) {
-
-      this.featuresPopupsProperties = {
-        'id': feature.getId(),
-        'name': feature.get('name'),
-        'layer_id': feature.get('layer_id'),
-        'geom_type': feature.get('geom_type'),
-        'created_at': feature.get('created_at'),
-        'updated_at': feature.get('updated_at'),
-        'fill_color': feature.get('fill_color'),
-        'stroke_color': feature.get('stroke_color'),
-        'stroke_width': feature.get('stroke_width'),
-        'wkt': getWkt(geomFeature)
-      }
-    }
+  hidePopup(): void {
+    this.displayPopup = false;
   }
 
   moveToastsFeaturesToBody(): void {
@@ -151,6 +119,13 @@ export class FeatureComponent implements OnInit {
     this.feature.set("stroke_color", color, true)
   }
 
+  zoomToFeature(): void {
+    this.currentLayer.zoomToFeature(this.feature.getId())
+  }
+
+  getWktFromFeature(feature: any): string {
+    return getWkt(feature.getGeometry())
+  }
 }
 
 
