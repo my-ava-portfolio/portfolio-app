ca3d37172857fc4988966992452aa03dea08ee0d
amaury <amauryval@gmail.com>
2022-09-04 23:14:16 +0200

    upd (colors.ts) colors updated

--- src/app/core/colors.ts
+++ src/app/core/colors.ts
@@ -1,8 +1,7 @@
-// should match with app.component.scss
+// should match with variables.scss
 
 // used by openlayers style (it cannot use scss)
-export const jobColor = 'rgba(225, 0, 1, 0.6)';
-export const educationColor = 'rgba(0, 144, 29, 0.6)';
-export const volunteerColor = 'rgba(98, 0, 255, 0.6)';
-const projectPersonalColor = 'rgba(255, 127, 0, 0.6)';
-
+export const jobColor = 'rgba(167, 10, 12, 0.8)';
+export const educationColor = 'rgba(42, 136, 35, 0.8)';
+export const volunteerColor = 'rgba(106, 61, 154, 0.8)';
+const projectPersonalColor = 'rgba(216, 112, 42, 0.8)';
