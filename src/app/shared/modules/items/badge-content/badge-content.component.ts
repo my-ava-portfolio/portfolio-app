fc91396819ef474d742b744fd930a61dbf80f002
amaury <amauryval@gmail.com>
2022-09-04 22:57:18 +0200

    upd (scss) badge style improved

--- src/app/shared/modules/items/badge-content/badge-content.component.ts
+++ src/app/shared/modules/items/badge-content/badge-content.component.ts
@@ -17,4 +17,25 @@ export class BadgeContentComponent implements OnInit {
   ngOnInit(): void {
   }
 
+
+  getConstract(hexColor: string): 'black' | 'white' {
+    const rgbValues: string[] | null = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hexColor)
+
+    if (rgbValues) {
+      const red = parseInt(rgbValues[1]);
+      const green = parseInt(rgbValues[2]);
+      const blue = parseInt(rgbValues[3]);
+  
+      const contrastLimit = Math.round((red * 299 + green * 587 + blue * 114) / 1000);
+  
+      if (contrastLimit > 125) {
+        return 'black';
+      } else {
+        return 'white';
+      }
+    }
+
+    return 'white'
+  }
+
 }
