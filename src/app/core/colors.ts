e920ca5a4672c3b0339c9563c94918dd659e490f
amaury <amauryval@gmail.com>
2022-09-05 21:08:02 +0200

    upd (*) colors improved

--- src/app/core/colors.ts
+++ src/app/core/colors.ts
@@ -1,7 +1,15 @@
 // should match with variables.scss
 
 // used by openlayers style (it cannot use scss)
-export const jobColor = 'rgba(167, 10, 12, 0.8)';
-export const educationColor = 'rgba(42, 136, 35, 0.8)';
-export const volunteerColor = 'rgba(106, 61, 154, 0.8)';
-const projectPersonalColor = 'rgba(216, 112, 42, 0.8)';
+export const jobColor = addAlpha('#e41a1c', 0.8);
+export const educationColor = addAlpha('#4daf4a', 0.8);
+export const volunteerColor = addAlpha('#984ea3', 0.8);
+const projectPersonalColor = addAlpha('#ff7f00', 0.8);
+
+
+
+function addAlpha(color: string, opacity: number): string {
+    //https://stackoverflow.com/questions/50890241/programmatically-add-opacity-to-a-color-in-typescript
+    const opacityValue = Math.round(Math.min(Math.max(opacity || 1, 0), 1) * 255);
+    return color + opacityValue.toString(16).toUpperCase();
+}
