39ed574959be1bada4575bc995c6214060b10deb
amaury <amauryval@gmail.com>
2022-09-06 21:25:43 +0200

    upd (button-component) improved

--- src/app/shared/modules/items/button-content/button-content.component.ts
+++ src/app/shared/modules/items/button-content/button-content.component.ts
@@ -7,8 +7,6 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class ButtonContentComponent implements OnInit {
   @Input() color!: string;
-  @Input() title!: string;
-
   @Input() route?: string;
   @Input() url?: string;
 
