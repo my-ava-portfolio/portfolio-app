d91a1d16f38b9117ce66b5d146d509607599238f
amaury <amauryval@gmail.com>
2022-12-23 21:49:21 +0100

    upd (education) remove useless code

--- src/app/modules/education/layout/layout.component.ts
+++ src/app/modules/education/layout/layout.component.ts
@@ -1,10 +1,6 @@
-import { Component, OnInit, OnDestroy, AfterViewInit, ViewEncapsulation  } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
-import { ResumeService } from '@services/resume.service';
 import { ControlerService } from '@services/controler.service';
-
-import { interval, Subscription } from 'rxjs';
-import { startWith  } from 'rxjs/operators';
 
 import { ActivatedRoute } from '@angular/router';
 import { Title } from '@angular/platform-browser';
@@ -26,11 +22,6 @@ export class LayoutComponent implements OnInit {
 
   educationTopics = educationPages;
 
-  isAnchorExistsChecker = interval(1000); // observable which run all the time
-  isAnchorExistsCheckerSubscription!: Subscription;
-
-  titleSubscription!: Subscription;
-
   constructor(
     private controlerService: ControlerService,
     private activatedRoute: ActivatedRoute,
@@ -45,43 +36,8 @@ export class LayoutComponent implements OnInit {
     this.sendResumeSubMenus()
   }
 
-  ngAfterViewInit(): void {
-    // TODO globally ?
-    this.activatedRoute.fragment.subscribe(
-      (fragment) => {
-        if (fragment === undefined) {
-          this.fragment = null;
-        } else {
-          this.fragment = fragment;
-        }
-
-        if (this.fragment !== null) {
-          this.checkAndScrollToAnchorIfNeeded();
-        }
-      }
-    );
-  }
-
-  ngOnDestroy(): void {
-  }
-
   sendResumeSubMenus(): void {
     this.controlerService.pullSubMenus(this.educationTopics.sub_menus)
-  }
-
-  checkAndScrollToAnchorIfNeeded(): void {
-
-    this.isAnchorExistsCheckerSubscription = this.isAnchorExistsChecker.pipe(startWith(0)).subscribe(() => {
-      try {
-        if (this.fragment !== null) {
-          const element: any = window.document.getElementById(this.fragment);
-          element.scrollIntoView();
-        } else {
-        }
-        this.isAnchorExistsCheckerSubscription.unsubscribe();
-      } catch (e) {
-      }
-    });
   }
 
 }
