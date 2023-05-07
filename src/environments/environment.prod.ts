caf29976507cb2646e5ec00930d0b513fadb1c5b
amaury <amauryval@gmail.com>
2023-05-07 18:27:21 +0200

    upd (environments) syntax

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,8 +5,8 @@ const gtfsVierwerApiSubdomain = "gtfs-viewer";
 const networkApiSubdomain = "network";
 
 export const environment = {
-  production: true,
-  resumeApiUrl: protocol + '://' + resumeApiSubdomain + domain + 'api/v1/',
-  gtfsViewerApiUrl: protocol + '://' + gtfsVierwerApiSubdomain + domain + 'api/v2/',
-  networkApiUrl: protocol + '://' + networkApiSubdomain + domain + 'api/v1/'
+  production: false,
+  resumeApiUrl: protocol + '://' + resumeApiSubdomain + domain + '/api/v1/',
+  gtfsViewerApiUrl: protocol + '://' + gtfsVierwerApiSubdomain + domain + '/api/v2/',
+  networkApiUrl: protocol + '://' + networkApiSubdomain + domain + '/api/v1/'
 };
